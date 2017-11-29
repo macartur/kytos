38128cdea830c3463d8f342672faf5a98bf782ae
Macartur Sousa <macartur.sc@gmail.com>
2017-11-28 22:25:46 -0200

    Add test to test python-openflow instalation

--- tests/test_system/test_container.py
+++ tests/test_system/test_container.py
@@ -1,4 +1,5 @@
 """Test connectivity between to hosts in Mininet."""
+import os
 from unittest import TestCase
 
 import pexpect
@@ -6,7 +7,7 @@ import pexpect
 CONTAINER = 'kytos_tests'
 IMAGE = 'kytos/systests'
 PROMPT = 'root@.*:/usr/local/src/kytos# '
-WITH_SUDO = True
+WITH_SUDO = True if os.geteuid() == 0 else False
 
 
 class TestStruct(TestCase):
@@ -17,9 +18,10 @@ class TestStruct(TestCase):
         """Execute command inside the bash"""
         if with_sudo:
             command = 'sudo ' + command
-        cls._kytos = pexpect.spawn(command)
+        terminal = pexpect.spawn(command)
         if expected is not None:
-            cls._kytos.expect(expected, timeout=timeout)
+            terminal.expect(expected, timeout=timeout)
+        return terminal
 
     @classmethod
     def setUpClass(cls):
@@ -39,13 +41,29 @@ class TestStruct(TestCase):
 
         # Start the container to run the tests
         cmd = f'docker run --rm -it --name {CONTAINER} {IMAGE}'
-        cls.execute(cmd, PROMPT, with_sudo=WITH_SUDO)
+        cls._kytos = cls.execute(cmd, PROMPT, with_sudo=WITH_SUDO)
 
     def test000_uname_a(self):
         """Test expected 'uname -a' command using the container."""
         expected = ["Linux", "4.13.0-1-amd64", "#1 SMP Debian 4.13.4-2"]
         self._kytos.sendline('uname -a')
         self._kytos.expect(expected)
+        self._kytos.expect(PROMPT)
+
+    def test00_update_repositories(self):
+        """Update all repositories."""
+        self._kytos.sendline('kytos-update')
+        self._kytos.expect(['Fast-forward', 'up-to-date'])
+        self._kytos.expect(PROMPT)
+
+    def test01_install_projects(self):
+        """Install Kytos projects from cloned repository in safe order."""
+        pip = 'pip install --no-index --find-links $KYTOSDEPSDIR .'
+        PROJECTS = ['python-openflow']
+        for project in PROJECTS:
+            self._kytos.sendline(f'cd $KYTOSDIR/{project}; {pip}; cd -')
+            self._kytos.expect(f'Successfully installed .*{project}', timeout=30)
+            self._kytos.expect(PROMPT)
 
     @classmethod
     def tearDownClass(cls):
